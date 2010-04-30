939c5c72ea96524385dc04803b6d2e6a66b8ecf3
mzp <mzpppp@gmail.com>
2010-04-30 10:07:19 +0900

    add implicit parameter to mkMem

--- object.v
+++ object.v
@@ -1,61 +1,61 @@
-Require Import Sets.Finite_sets.
 Require Import Sets.Ensembles.
-Inductive mark :=
-  | Marked : mark
-  | Unmarked : mark.
 
+(* operations of set  *)
+Definition set A := Ensemble A.
 Definition In {A : Type} elem sets := Sets.Ensembles.In A sets elem.
 Implicit Arguments Sets.Ensembles.Included [U].
-Implicit Arguments Sets.Ensembles.Add [U].
 Implicit Arguments Sets.Ensembles.Union [U].
-Implicit Arguments Sets.Ensembles.Intersection [U].
-Implicit Arguments Sets.Finite_sets.Finite [U].
 
-Record Mem (A : Type) := mkMem {
-  roots   : Ensemble A;
-  nodes   : Ensemble A;
-  frees   : Ensemble A;
-  marker  : A -> mark;
-  pointer : A -> option A
+(* definition of memory *)
+Inductive mark :=
+  | Marked   : mark
+  | Unmarked : mark.
+
+Record Mem {A : Type} := mkMem {
+  roots   : set A;          (* root objects *)
+  nodes   : set A;          (* all objects in memory *)
+  frees   : set A;          (* free list *)
+  marker  : A -> mark;      (* get mark of the object *)
+  pointer : A -> option A   (* get next object of the object *)
 }.
-Implicit Arguments roots [A].
-Implicit Arguments nodes [A].
-Implicit Arguments frees [A].
-Implicit Arguments marker [A].
-Implicit Arguments pointer [A].
-Implicit Arguments mkMem [A].
 
-Inductive Closure (A : Type) (pointer : A -> option A) (root : A) : Ensemble A :=
-  | CRefl  : In root (Closure A pointer root)
-  | CTrans : forall n m, Some n = pointer root -> In m (Closure A pointer n) -> In m (Closure A pointer root).
+(* Definition of GC *)
 
-Inductive Closures (A : Type) (pointer : A -> option A) (roots : Ensemble A) : Ensemble A :=
-  | Closures_intro  : forall n m, In m roots -> In n (Closure A pointer m) -> In n (Closures A pointer roots).
+(* [Closure A next x] means a set: {x, next x, next (next x), ... }. *)
+Inductive Closure (A : Type) (next : A -> option A) : A -> set A :=
+  | CRoot  : forall x,
+    In x (Closure A next x)
+  | CTrans : forall x y z,
+    Some y = next x -> In z (Closure A next y) -> In z (Closure A next x).
+
+Inductive Closures (A : Type) (next : A -> option A) (roots : set A) : set A :=
+  | Closures_intro  : forall n m,
+    In m roots -> In n (Closure A next m) -> In n (Closures A next roots).
 Implicit Arguments Closures [A].
 
-Inductive Marks (A : Type) (marker : A -> mark) (m : mark) (xs : Ensemble A) : Ensemble A :=
+Inductive Marks (A : Type) (marker : A -> mark) (m : mark) (xs : set A) : set A :=
   | Marks_intro : forall x, In x xs -> marker x = m -> In x (Marks A marker m xs).
 
-Definition Marker {A : Type} (m1 m2 : Mem A) : Prop :=
+Definition Marker {A : Type} (m1 m2 : Mem) : Prop :=
   roots   m1 = roots   m2 /\
   nodes   m1 = nodes   m2 /\
   frees   m1 = frees   m2 /\
   pointer m1 = pointer m2 /\
   Included (Closures (pointer m2) (roots m2)) (Marks A (marker m2) Marked (nodes m2)).
 
-Definition Sweeper {A : Type} (m1 m2 : Mem A) : Prop :=
+Definition Sweeper {A : Type} (m1 m2 : Mem) : Prop :=
   roots   m1 = roots   m2 /\
   nodes   m1 = nodes   m2 /\
   pointer m1 = pointer m2 /\
   frees   m2 = Union (frees m1) (Marks A (marker m1) Unmarked (nodes m1)) /\
   forall n, In n (nodes m2) -> marker m2 n = Unmarked.
 
-Definition invariant {A : Type} (m : Mem A) : Prop :=
+Definition invariant {A : Type} (m : Mem) : Prop :=
   Included (roots m) (nodes m) /\
   Included (frees m) (nodes m) /\
-  forall x y, In x (nodes m) -> Some y = pointer m x -> In y (nodes m).
+  forall (x y :A), In x (nodes m) -> Some y = pointer m x -> In y (nodes m).
 
-Lemma marker_invariant : forall A (m1 m2 : Mem A),
+Lemma marker_invariant : forall A (m1 m2 : Mem (A:=A)),
   Marker m1 m2 -> invariant m1 -> invariant m2.
 Proof.
 unfold Marker, invariant.
@@ -75,7 +75,7 @@ inversion H.
 tauto.
 Qed.
 
-Lemma sweeper_invariant: forall A (m1 m2 : Mem A),
+Lemma sweeper_invariant: forall A (m1 m2 : Mem (A:=A)),
   Sweeper m1 m2 -> invariant m1 -> invariant m2.
 Proof.
 unfold Sweeper, invariant.
@@ -94,13 +94,13 @@ apply (H12 _ _ (marker m1) Unmarked _).
 tauto.
 Qed.
 
-Definition frees_invariant {A : Type} (m : Mem A) :=
+Definition frees_invariant {A : Type} (m : Mem) :=
   Disjoint A (frees m) (Closures (pointer m) (roots m)).
 
-Definition marks_all {A : Type} (m : Mem A) :=
+Definition marks_all {A : Type} (m : Mem) :=
   Disjoint A (Marks A (marker m) Unmarked (nodes m)) (Closures (pointer m) (roots m)).
 
-Lemma sweeper_frees : forall A (m1 m2 : Mem A),
+Lemma sweeper_frees : forall A (m1 m2 : Mem (A:=A)),
   frees_invariant m1 -> marks_all m1 -> Sweeper m1 m2 -> frees_invariant m2.
 Proof.
 unfold frees_invariant, marks_all, Sweeper.
@@ -122,7 +122,7 @@ inversion H10.
  apply Intersection_intro; auto.
 Qed.
 
-Lemma disjoint_include: forall A (B C D : Ensemble A),
+Lemma disjoint_include: forall A (B C D : set A),
   Disjoint A B C -> Included D C -> Disjoint A B D.
 Proof.
 intros.
@@ -135,7 +135,7 @@ apply Intersection_intro; auto.
 Qed.
 
 
-Lemma marker_frees : forall A (m1 m2 : Mem A),
+Lemma marker_frees : forall A (m1 m2 : Mem (A:=A)),
   frees_invariant m1 -> Marker m1 m2 -> frees_invariant m2 /\ marks_all m2.
 Proof.
 unfold frees_invariant, Marker, marks_all.
@@ -156,9 +156,9 @@ split; intros.
  inversion H14.
 Qed.
 
-Definition GC {A : Type} (m1 m2 : Mem A) := exists m : Mem A,
+Definition GC {A : Type} (m1 m2 : Mem (A:=A)) := exists m : Mem,
   Marker m1 m /\ Sweeper m m2.
-Theorem gc_invariant : forall A (m1 m2 : Mem A),
+Theorem gc_invariant : forall A (m1 m2 : Mem (A:=A)),
   invariant m1 -> GC m1 m2 -> invariant m2.
 Proof.
 unfold GC.
@@ -173,7 +173,7 @@ decompose [ex and] H0.
  tauto.
 Qed.
 
-Theorem gc_free: forall A (m1 m2 : Mem A),
+Theorem gc_free: forall A (m1 m2 : Mem (A:=A)),
   frees_invariant m1 -> GC m1 m2 -> frees_invariant m2.
 Proof.
 unfold GC.
